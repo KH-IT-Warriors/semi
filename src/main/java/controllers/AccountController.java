0d57e12a0ae353f8506abb9ac5b02e86327aa505
Jang-Luke <lujke555@gmail.com>
2023-04-21 17:47:26 +0900

    feat: Creating simulated table & insert account
    가상 테이블 작성 및 insert 생성
    테스트 완료

---
+++ src/main/java/controllers/AccountController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import domains.account.management.AccountInformation;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class AccountController {
+
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection("url", "id", "pw");
+    }
+
+
+//      "CREATE TABLE USER_ACCOUNT (
+//          ACCOUNT_ID BIGINT AUTO_INCREMENT,
+//          ACCOUNT_STATUS_ID BIGINT,
+//          ACCOUNT_ROLE_ID BIGINT,
+//          USER_NAME VARCHAR(255) NOT NULL,
+//          USER_PW CHAR(256) NOT NULL,
+//          PRIMARY KEY (ACCOUNT_ID),
+//          FOREIGN KEY (ACCOUNT_STATUS_ID) REFERENCES ACCOUNT_STATUSES (ACCOUNT_STATUS_ID),
+//          FOREIGN KEY (ACCOUNT_ROLE_ID) REFERENCES ACCOUNT_ROLES (ACCOUNT_ROLE_ID)
+//      );"
+    // 계정
+    public void insertAccount(AccountInformation accountInformation) throws Exception {
+        String sql = "INSERT INTO USER_ACCOUNTS VALUES(0, ?, ?, ?, ?)";
+//        try(Connection connection = getConnection();
+//            PreparedStatement preparedStatement = connection.prepareStatement(sql);){
+//            preparedStatement.setLong(1, accountInformation.getAccountStatusId());
+//            preparedStatement.setLong(2, accountInformation.getAccountRoleId());
+//            preparedStatement.setString(3, accountInformation.getUserName());
+//            preparedStatement.setString(4, accountInformation.getUserPw());
+//            preparedStatement.executeUpdate();
+            System.out.println(sql);
+            System.out.println("계정 아이디 : "+accountInformation.getAccountStatusId());
+            System.out.println("계정 상태 아이디 : " + accountInformation.getAccountStatusId());
+            System.out.println("역할 아이디 : " + accountInformation.getAccountRoleId());
+            System.out.println("사용자 아이디 : " + accountInformation.getUserName());
+            System.out.println("비밀번호 : " + accountInformation.getUserPw());
+//        }
+    }
+}
+
--- src/main/java/kr/co/khacademy/semi/Main.java
+++ src/main/java/kr/co/khacademy/semi/Main.java
@@ -1,8 +1,18 @@
 package kr.co.khacademy.semi;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("asd");
+import controllers.AccountController;
+import domains.account.management.AccountInformation;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AccountInformation accountInformation = AccountInformation.builder()
+            .accountId(100)
+            .accountStatusId(200)
+            .accountRoleId(5)
+            .userName("길웅")
+            .userPw("123")
+            .build();
+        AccountController accountController = new AccountController();
+        accountController.insertAccount(accountInformation);
     }
 }
