0c3c476e30395ad671de3f2feee67f951201ed46
Jang-Luke <lujke555@gmail.com>
2023-04-23 19:50:23 +0900

    feat: Creating findAccountInformationById method
    account_id 로 계정 정보를 찾는 메서드 구현.
    실제 테이블이 없어 테스트 진행 불가
    account_id 가 존재하지 않을 시 UserNotFoundException 예외 발생
    -> 예외 클래스는 아직 만들지 않음. 추후 컨펌 예정 (실질적으로 필요한 에러인지 아닌지)

--- src/main/java/controllers/AccountController.java
+++ src/main/java/controllers/AccountController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import domains.account.commons.Password;
 import domains.account.management.AccountInformation;
 
 import java.sql.Connection;
@@ -16,7 +17,7 @@ public class AccountController {
 
 
 //      "CREATE TABLE USER_ACCOUNT (
-//          ACCOUNT_ID BIGINT AUTO_INCREMENT,
+//          ID BIGINT AUTO_INCREMENT,
 //          ACCOUNT_STATUS_ID BIGINT,
 //          ACCOUNT_ROLE_ID BIGINT,
 //          USER_NAME VARCHAR(255) NOT NULL,
@@ -33,10 +34,10 @@ public class AccountController {
 //            preparedStatement.setLong(1, accountInformation.getAccountStatusId());
 //            preparedStatement.setLong(2, accountInformation.getAccountRoleId());
 //            preparedStatement.setString(3, accountInformation.getUserName());
-//            preparedStatement.setString(4, accountInformation.getUserPw());
+//            preparedStatement.setString(4, accountInformation.getUserPw().toString());
 //            preparedStatement.executeUpdate();
             System.out.println(sql);
-            System.out.println("계정 아이디 : "+accountInformation.getAccountStatusId());
+            System.out.println("계정 아이디 : "+accountInformation.getAccountId());
             System.out.println("계정 상태 아이디 : " + accountInformation.getAccountStatusId());
             System.out.println("역할 아이디 : " + accountInformation.getAccountRoleId());
             System.out.println("사용자 아이디 : " + accountInformation.getUserName());
@@ -44,10 +45,10 @@ public class AccountController {
 //        }
     }
     public void updateAccount(AccountInformation accountInformation) throws Exception {
-        String sql = "UPDATE USER_ACCOUNTS SET USER_PW = ? WHERE ACCOUNT_ID = ?";
+        String sql = "UPDATE USER_ACCOUNTS SET USER_PW = ? WHERE ID = ?";
 //        try(Connection connection = getConnection();
 //            PreparedStatement preparedStatement = connection.prepareStatement(sql);){
-//            preparedStatement.setString(1, accountInformation.getUserPw());
+//            preparedStatement.setString(1, accountInformation.getUserPw().withPassword("변경할 Pw"));
 //            preparedStatement.setLong(2, accountInformation.getAccountId());
 //            preparedStatement.executeUpdate();
             System.out.println(sql);
@@ -58,7 +59,7 @@ public class AccountController {
     }
 
     public void deleteAccount(AccountInformation accountInformation) throws Exception {
-        String sql = "UPDATE USER_ACCOUNTS SET ACCOUNT_STATUS = ? WHERE ACCOUNT_ID = ?";
+        String sql = "UPDATE USER_ACCOUNTS SET ACCOUNT_STATUS = ? WHERE ID = ?";
 //        try(Connection connection = getConnection();
 //            PreparedStatement preparedStatement = connection.prepareStatement(sql);){
 //            preparedStatement.setLong(1, 2L); // 삭제대기 상태 아이디
@@ -69,5 +70,32 @@ public class AccountController {
 //        }
     }
 
+    // 테이블이 실존하지 않아 테스트 불가능
+    public AccountInformation findAccountInformationById(AccountInformation accountInformation) throws Exception {
+        String sql = "SELECT * FROM USER_ACCOUNTS WHERE ID = ?";
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);){
+            preparedStatement.setLong(1, accountInformation.getAccountId());
+            try(ResultSet resultSet = preparedStatement.executeQuery();){
+                if (resultSet.next()) {
+                    long accountId = resultSet.getLong("ID");
+                    long accountStatusId = resultSet.getLong("ACCOUNT_STATUS_ID");
+                    long accountRoleId = resultSet.getLong("ACCOUNT_ROLE_ID");
+                    String userName = resultSet.getString("USER_NAME");
+                    String userPw = resultSet.getString("USER_PW");
+                    return AccountInformation.builder()
+                        .accountId(accountId)
+                        .accountStatusId(accountStatusId)
+                        .accountRoleId(accountRoleId)
+                        .userName(userName)
+                        .userPw(Password.of(accountId, userPw))
+                        .build();
+                } else {
+                    throw new RuntimeException();
+//                    throw new UserNotFoundException("사용자 아이디가 일치하지 않습니다.");
+                }
+            }
+        }
+    }
 }
 
--- src/main/java/domains/account/commons/Password.java
+++ src/main/java/domains/account/commons/Password.java
@@ -5,7 +5,7 @@ import lombok.NonNull;
 import lombok.Value;
 import lombok.With;
 
-@Value(staticConstructor = "of")
+//@Value(staticConstructor = "of")
 public class Password {
     @NonNull
     long accountId;
@@ -23,7 +23,17 @@ public class Password {
         this.password = password;
     }
 
+    public static Password of(@NonNull long accountId, @NonNull String password) {
+        return new Password(accountId, password);
+    }
+
     private boolean validatesPassword(String password) {
         return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()\\-=_+\\[\\]{};':\\\"\\\\|,.<>/?]).{8,16}$");
+    }
+
+    // 테스트용 코드, 추후 수정 혹은 삭제 예정
+    @Override
+    public String toString(){
+        return this.password;
     }
 }
