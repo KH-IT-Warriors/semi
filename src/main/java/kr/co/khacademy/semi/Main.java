75ec270cd554b6dfb9f0995fedfad34fbac009e9
Jang-Luke <lujke555@gmail.com>
2023-04-23 19:50:23 +0900

    feat: Creating findAccountInformationById method
    account_id 로 계정 정보를 찾는 메서드 구현.
    실제 테이블이 없어 테스트 진행 불가
    account_id 가 존재하지 않을 시 UserNotFoundException 예외 발생
    -> 예외 클래스는 아직 만들지 않음. 추후 컨펌 예정 (실질적으로 필요한 에러인지 아닌지)

--- src/main/java/kr/co/khacademy/semi/Main.java
+++ src/main/java/kr/co/khacademy/semi/Main.java
@@ -1,27 +1,47 @@
 package kr.co.khacademy.semi;
 
 import controllers.AccountController;
+import domains.account.commons.Password;
 import domains.account.management.AccountInformation;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        AccountController accountController = new AccountController();
+
         AccountInformation accountInformation1 = AccountInformation.builder()
             .accountId(100)
             .accountStatusId(200)
-            .accountRoleId(5)
+            .accountRoleId(1)
             .userName("길웅")
-            .userPw("123")
+            .userPw(Password.of(100, "qweR!234"))
             .build();
-        AccountController accountController = new AccountController();
         accountController.insertAccount(accountInformation1);
 
+        System.out.println("===============");
+
         AccountInformation accountInformation2 = AccountInformation.builder()
-            .accountId(123)
+            .accountId(100)
             .accountStatusId(200)
-            .accountRoleId(5)
+            .accountRoleId(1)
             .userName("길웅")
-            .userPw("456")
+            .userPw(Password.of(100, "qweR!234").withPassword("QWer123$"))
             .build();
         accountController.updateAccount(accountInformation2);
+
+        System.out.println("===============");
+
+        AccountInformation accountInformation3 = AccountInformation.builder()
+            .accountId(100)
+            .accountStatusId(205)
+            .accountRoleId(5)
+            .userName("길웅")
+            .userPw(Password.of(100, "QWEr123$"))
+            .build();
+        accountController.deleteAccount(accountInformation3);
+
+        System.out.println("===============");
+
+        AccountInformation user = accountController.findAccountInformationById(accountInformation1);
+        System.out.println(user);
     }
 }
