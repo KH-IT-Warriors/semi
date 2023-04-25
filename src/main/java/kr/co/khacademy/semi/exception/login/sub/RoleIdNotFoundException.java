ae758aed6ff7b4e255a5a6830d715af9521b5b03
Jang-Luke <lujke555@gmail.com>
2023-04-25 14:41:46 +0900

    refactor: Exception overloading

--- src/main/java/kr/co/khacademy/semi/exception/login/sub/RoleIdNotFoundException.java
+++ src/main/java/kr/co/khacademy/semi/exception/login/sub/RoleIdNotFoundException.java
@@ -3,4 +3,11 @@ package kr.co.khacademy.semi.exception.login.sub;
 import kr.co.khacademy.semi.exception.login.LoginFailedException;
 
 public class RoleIdNotFoundException extends LoginFailedException {
+    public RoleIdNotFoundException() {
+        super();
+    }
+
+    public RoleIdNotFoundException(String message) {
+        super(message);
+    }
 }
--- src/main/java/kr/co/khacademy/semi/exception/login/sub/UsernameNotFoundException.java
+++ src/main/java/kr/co/khacademy/semi/exception/login/sub/UsernameNotFoundException.java
@@ -3,4 +3,11 @@ package kr.co.khacademy.semi.exception.login.sub;
 import kr.co.khacademy.semi.exception.login.LoginFailedException;
 
 public class UsernameNotFoundException extends LoginFailedException {
+    public UsernameNotFoundException() {
+        super();
+    }
+
+    public UsernameNotFoundException(String message) {
+        super(message);
+    }
 }
