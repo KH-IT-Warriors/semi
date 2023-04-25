0d374ec694204ebc1b24442e10f7135ab1238e41
Jang-Luke <lujke555@gmail.com>
2023-04-25 20:45:45 +0900

    feat: Create UserGrade class
    사용자 등급 entity 객체 생성
    이름 및 등급 종류는 추후 수정 예정

---
+++ src/main/java/kr/co/khacademy/semi/entity/UserGrade.java
@@ -0,0 +1,10 @@
+package kr.co.khacademy.semi.entity;
+
+import lombok.Data;
+
+@Data
+public class UserGrade {
+    private String grade1;
+    private String grade2;
+    private String grade3;
+}
