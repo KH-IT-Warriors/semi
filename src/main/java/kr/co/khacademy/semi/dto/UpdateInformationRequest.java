57dc424f0e46bbdbae04b42aa7bfe1042a26581b
Jang-Luke <lujke555@gmail.com>
2023-04-26 11:41:31 +0900

    refactor: Refactor validate process
    회원정보 유효성 검사 코드가 과하게 반복되는 문제가 있어 별도의 객체로 분리

--- src/main/java/kr/co/khacademy/semi/dto/UpdateInformationRequest.java
+++ src/main/java/kr/co/khacademy/semi/dto/UpdateInformationRequest.java
@@ -1,5 +1,6 @@
 package kr.co.khacademy.semi.dto;
 
+import kr.co.khacademy.semi.common.validator.AccountDataValidator;
 import kr.co.khacademy.semi.exception.join.sub.InvalidEmailException;
 import kr.co.khacademy.semi.exception.join.sub.InvalidPhoneNumberException;
 import lombok.Data;
@@ -7,6 +8,8 @@ import lombok.Value;
 
 @Data
 public class UpdateInformationRequest {
+    AccountDataValidator accountDataValidator = AccountDataValidator.getInstance();
+
     private Long accountId;
     private String name;
     private String phoneNumber;
@@ -17,12 +20,12 @@ public class UpdateInformationRequest {
         this.accountId = accountId;
         this.name = name;
 
-        if (!validatesPhoneNumber(phoneNumber)) {
+        if (!accountDataValidator.validatesPhoneNumber(phoneNumber)) {
             throw new InvalidPhoneNumberException("잘못된 형식의 전화번호입니다.");
         }
         this.phoneNumber = phoneNumber;
 
-        if (!validatesEmail(email)) {
+        if (!accountDataValidator.validatesEmail(email)) {
             throw new InvalidEmailException("잘못된 형식의 이메일입니다.");
         }
         this.email = email;
@@ -30,21 +33,5 @@ public class UpdateInformationRequest {
 
     public static UpdateInformationRequest of(Long accountId, String name, String phoneNumber, String email) {
         return new UpdateInformationRequest(accountId, name, phoneNumber, email);
-    }
-
-    private boolean validatesPhoneNumber(String phoneNumber) {
-        String phoneNumberForm = "^01\\d\\d{8}$";
-        if (phoneNumber.matches(phoneNumberForm)) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean validatesEmail(String email) {
-        String emailForm = "^[a-z0-9]+@.+(\\.com)$";
-        if (email.matches(emailForm)) {
-            return true;
-        }
-        return false;
     }
 }
--- src/main/java/kr/co/khacademy/semi/dto/UpdatePasswordRequest.java
+++ src/main/java/kr/co/khacademy/semi/dto/UpdatePasswordRequest.java
@@ -1,15 +1,18 @@
 package kr.co.khacademy.semi.dto;
 
+import kr.co.khacademy.semi.common.validator.AccountDataValidator;
 import kr.co.khacademy.semi.exception.join.sub.InvalidPasswordException;
 import lombok.Data;
 import lombok.NonNull;
 
 @Data
 public class UpdatePasswordRequest {
+    AccountDataValidator accountDataValidator = AccountDataValidator.getInstance();
+
     @NonNull
     private Long id;
     @NonNull
-    private String password;
+    private String plainPassword;
 
     boolean pwHasDownCase;
     boolean pwHasUpperCase;
@@ -17,52 +20,18 @@ public class UpdatePasswordRequest {
     boolean pwHasSpecialWord;
     boolean pwBetween8And15;
 
-    private UpdatePasswordRequest(Long id, String password) {
+    private UpdatePasswordRequest(Long id, String plainPassword) {
         this.id = id;
 
-        if (!validatesPassword(password)) {
+        if (!accountDataValidator.validatesPassword(plainPassword)) {
             InvalidPasswordException invalidPasswordException = new InvalidPasswordException("잘못된 형식의 비밀번호입니다.");
             invalidPasswordException.printStackTrace();
             throw new InvalidPasswordException();
         }
-        this.password = password;
+        this.plainPassword = plainPassword;
     }
 
     public static UpdatePasswordRequest of(Long id, String password) {
         return new UpdatePasswordRequest(id, password);
-    }
-
-    private boolean validatesPassword(String plainPassword) {
-        String hasDownCase = ".*[a-z].*?$";
-        if (plainPassword.matches(hasDownCase)) {
-            this.pwHasDownCase = true;
-        } else {
-            return false;
-        }
-        String hasUpperCase = ".*[A-Z].*?$";
-        if (plainPassword.matches(hasUpperCase)) {
-            this.pwHasUpperCase = true;
-        } else {
-            return false;
-        }
-        String hasNumber = ".*[0-9].*?$";
-        if (plainPassword.matches(hasNumber)) {
-            this.pwHasNumber = true;
-        } else {
-            return false;
-        }
-        String hasSpecialWord = ".*[!@#$%^&*()\\-=_+\\[\\]{};':\\\"\\\\|,.<>/?].*?$";
-        if (plainPassword.matches(hasSpecialWord)) {
-            this.pwHasSpecialWord = true;
-        } else {
-            return false;
-        }
-        String between8And15 = ".{8,15}";
-        if (plainPassword.matches(between8And15)) {
-            this.pwBetween8And15 = true;
-        } else {
-            return false;
-        }
-        return true;
     }
 }
