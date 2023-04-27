a3b5d7b405ec1dd4a0c669097776463d4e649af4
mainlib990 <mainlib990@gmail.com>
2023-04-28 00:48:49 +0900

    Revert "기능 구현 틀 작성 (#10)"
    This reverts commit 487ffd3fcd3ffd46a9f9ed304f6c1c1ddbd4bcc2.

--- src/main/java/kr/co/khacademy/semi/common/encryption/PasswordEncryptionProvider.java
+++
@@ -1,6 +0,0 @@
-package kr.co.khacademy.semi.common.encryption;
-
-public interface PasswordEncryptionProvider {
-
-    boolean checkPassword(String plainPassword, String encryptedPassword);
-}
--- src/main/java/kr/co/khacademy/semi/service/AccountService.java
+++
@@ -1,8 +0,0 @@
-package kr.co.khacademy.semi.service;
-
-import kr.co.khacademy.semi.dto.LoginRequest;
-
-public interface AccountService {
-
-    Long login(LoginRequest loginRequest);
-}
