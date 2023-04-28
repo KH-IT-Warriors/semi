9dff9fc228aa6b642d57799a749b8a62738a3b2d
Jang-Luke <lujke555@gmail.com>
2023-04-28 12:12:48 +0900

    feat: 회원가입 컨트롤러에 DTO of(request) 구현

--- src/main/java/kr/co/khacademy/semi/controller/AccountController.java
+++ src/main/java/kr/co/khacademy/semi/controller/AccountController.java
@@ -3,6 +3,7 @@ package kr.co.khacademy.semi.controller;
 import kr.co.khacademy.semi.dao.AccountDao;
 import kr.co.khacademy.semi.dao.ProfileDao;
 import kr.co.khacademy.semi.model.Account;
+import kr.co.khacademy.semi.model.Password;
 import kr.co.khacademy.semi.model.Profile;
 
 import javax.servlet.ServletException;
@@ -11,26 +12,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Optional;
 
-@WebServlet("/my-page")
-public class MyPageController extends HttpServlet {
+@WebServlet("/account/*")
+public class AccountController extends HttpServlet {
 
     private static AccountDao accountDao = AccountDao.getInstance();
-    private static ProfileDao profileDao = ProfileDao.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Long id = (Long) req.getSession().getAttribute("accountId");
-        Optional<Account> myAccount = accountDao.read(id);
-        Optional<Profile> myProfile = profileDao.read(id);
-        req.setAttribute("myAccount", myAccount);
-        req.setAttribute("myProfile", myProfile);
-        req.getRequestDispatcher("/WEB-INF/views/my-page").forward(req, resp);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        String pathInfo = req.getPathInfo();
+        if ("/join".equals(pathInfo)) {
+            Account account = Account.of(req);
+            Password password = Password.of(req);
+            Profile profile = Profile.of(req);
+        }
     }
 }
+
+// TODO: ROLE 추가 필요.
