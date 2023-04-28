638896b23cbf9d2a9e9e2e19dc32ea32e11615ff
Jang-Luke <lujke555@gmail.com>
2023-04-28 17:35:41 +0900

    feat: 관리자 -> 관리자 계정 출력 기능
    일반 계정과 관리자 계정 리스트 분리 작업 추가

---
+++ src/main/java/kr/co/khacademy/semi/controller/AdminUserController.java
@@ -0,0 +1,50 @@
+package kr.co.khacademy.semi.controller;
+
+import kr.co.khacademy.semi.dao.UserDao;
+import kr.co.khacademy.semi.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@WebServlet("/admin/account/admin-user/*")
+public class AdminUserController extends HttpServlet {
+
+    private static final UserDao userDao = UserDao.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            String pathInfo = req.getPathInfo();
+            if ("/list".equals(pathInfo)) {
+                List<User> adminUsers = userDao.read();
+                adminUsers.stream()
+                    .filter((user) -> {
+                        return user.getAccount().getRoleId() != 1;
+                    })
+                    .collect(Collectors.toList()
+                );
+                req.setAttribute("adminUsers", adminUsers);
+                req.getRequestDispatcher("/WEB-INF/views/admin/account/admin_user/list.jsp");
+            } else if ("/item".equals(pathInfo)) {
+                Long id = Long.valueOf(req.getParameter("id"));
+                User adminUser = userDao.read(id);
+                req.setAttribute("adminUser", adminUser);
+                req.getRequestDispatcher("/WEB-INF/views/admin/account/admin_user/list.jsp");
+            }
+        } catch (SQLException e) {
+            resp.sendRedirect("/error.jsp");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
--- src/main/java/kr/co/khacademy/semi/controller/NormalUserController.java
+++ src/main/java/kr/co/khacademy/semi/controller/NormalUserController.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @WebServlet("/admin/account/normal-user/*")
 public class NormalUserController extends HttpServlet {
@@ -25,6 +26,12 @@ public class NormalUserController extends HttpServlet {
             String pathInfo = req.getPathInfo();
             if ("/list".equals(pathInfo)) {
                 List<User> normalUsers = userDao.read();
+                normalUsers.stream()
+                    .filter((user) -> {
+                        return user.getAccount().getRoleId() == 1;
+                    })
+                    .collect(Collectors.toList()
+                );
                 req.setAttribute("normalUsers", normalUsers);
                 req.getRequestDispatcher("/WEB-INF/views/admin/account/normal_user/list.jsp").forward(req, resp);
             } else if ("/item".equals(pathInfo)) {
@@ -40,11 +47,12 @@ public class NormalUserController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String pathInfo = req.getPathInfo();
         try {
+            String pathInfo = req.getPathInfo();
             if ("/modify".equals(pathInfo)) {
                 User user = User.of(req);
-                userDao.update(user);
+                Long targetId = Long.valueOf(req.getParameter("target-id"));
+                userDao.update(user, targetId);
             } else if ("/delete".equals(pathInfo)) {
                 Long targetId = Long.valueOf(req.getParameter("target-id"));
                 userDao.delete(targetId);
@@ -54,5 +62,3 @@ public class NormalUserController extends HttpServlet {
         }
     }
 }
-
-// TODO: ROLE 추가 필요.
