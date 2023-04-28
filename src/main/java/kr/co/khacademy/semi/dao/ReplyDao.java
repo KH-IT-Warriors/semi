2c9713eb077ab006b3bda99a736584ddc43d13df
KBJ <rkqudwns@naver.com>
2023-04-28 17:34:47 +0900

    ReplyDao 출력기능추가

--- src/main/java/kr/co/khacademy/semi/dao/ReplyDao.java
+++ src/main/java/kr/co/khacademy/semi/dao/ReplyDao.java
@@ -1,7 +1,6 @@
 package kr.co.khacademy.semi.dao;
 
 import kr.co.khacademy.semi.common.DataSource;
-import kr.co.khacademy.semi.model.Product;
 import kr.co.khacademy.semi.model.Reply;
 
 import java.sql.Connection;
@@ -9,23 +8,24 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ReplyDao {
 
     private static final ReplyDao instance = new ReplyDao();
 
-    private static final String INSERT_REPLY_SQL =
-        "INSERT INTO reply(parent_reply_id, account_id, contents) VALUES (?, ?, ?)";
+    private static final String INSERT_SQL =
+        "INSERT INTO reply VALUES (DEFAULT, ?, ?, ?, ?, DEFAULT)";
 
-    private static final String UPDATE_REPLY_SQL =
-        "UPDATE reply SET contents = ? ,created= DEFAULT WHERE id = ?";
-
-    private static final String DELETE_REPLY_SQL =
-        "DELETE FROM reply WHERE id = ?";
-
-    private static final String READ_REPLY_SQL =
+    private static final String SELECT_SQL =
         "SELECT * FROM reply";
+
+    private static final String UPDATE_BY_ID_SQL =
+        "UPDATE reply SET contents = ?, created = DEFAULT WHERE id = ?";
+
+    private static final String DELETE_BY_ID_SQL =
+        "DELETE FROM reply WHERE id = ?";
 
     private ReplyDao() {
     }
@@ -34,51 +34,50 @@ public class ReplyDao {
         return instance;
     }
 
-
     public void create(Reply reply) throws SQLException {
         try (Connection connection = DataSource.getConnection()) {
-            try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_REPLY_SQL)) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_SQL)) {
                 preparedStatement.setLong(1, reply.getParentReplyId());
                 preparedStatement.setLong(2, reply.getAccountId());
-                preparedStatement.setString(3, reply.getContents());
+                preparedStatement.setLong(3, reply.getProductId());
+                preparedStatement.setString(4, reply.getContents());
 
                 if (preparedStatement.executeUpdate() == 0) {
-                    connection.rollback();
                     throw new SQLException();
                 }
+                connection.commit();
             }
         }
     }
 
-    public Reply read(Long id) {
 
-    }
 
     public List<Reply> read() throws SQLException {
         try (Connection connection = DataSource.getConnection()) {
-            try (PreparedStatement preparedStatement = connection.prepareStatement(READ_REPLY_SQL);
-                 ResultSet resultSet = preparedStatement.executeQuery()) {
-
-                List<Reply> list = new ArrayList<>();
+            try (
+                PreparedStatement preparedStatement = connection.prepareStatement(SELECT_SQL);
+                ResultSet resultSet = preparedStatement.executeQuery()
+            ) {
+                List<Reply> replies = new ArrayList<>();
                 while (resultSet.next()) {
                     Reply reply = Reply.of(resultSet);
-                    list.add(reply);
+                    replies.add(reply);
                 }
-                return list;
+                return Collections.unmodifiableList(replies);
             }
         }
     }
 
     public void update(Reply reply) throws SQLException {
         try (Connection connection = DataSource.getConnection()) {
-            try (PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_REPLY_SQL)) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_BY_ID_SQL)) {
                 preparedStatement.setString(1, reply.getContents());
                 preparedStatement.setLong(2, reply.getId());
 
                 if (preparedStatement.executeUpdate() == 0) {
-                    connection.rollback();
                     throw new SQLException();
                 }
+                connection.commit();
             }
         }
 
@@ -86,14 +85,13 @@ public class ReplyDao {
 
     public void delete(Long id) throws SQLException {
         try (Connection connection = DataSource.getConnection()) {
-            try (PreparedStatement preparedStatement = connection.prepareStatement(DELETE_REPLY_SQL)) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(DELETE_BY_ID_SQL)) {
                 preparedStatement.setLong(1, id);
 
                 if (preparedStatement.executeUpdate() == 0) {
-                    connection.rollback();
                     throw new SQLException();
                 }
-
+                connection.commit();
             }
         }
     }
