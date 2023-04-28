03e0b4f243b567870ddff86f3f4588a5220a49df
dhanhak <dhanhak@naver.com>
2023-04-28 17:06:45 +0900

    Announcement read 구현

--- src/main/java/kr/co/khacademy/semi/dao/AnnouncementDao.java
+++ src/main/java/kr/co/khacademy/semi/dao/AnnouncementDao.java
@@ -2,39 +2,72 @@ package kr.co.khacademy.semi.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import kr.co.khacademy.semi.common.DataSource;
 import kr.co.khacademy.semi.model.Announcement;
 
 public class AnnouncementDao {
     private static final AnnouncementDao instance = new AnnouncementDao();
-    
+
     private static final String INSERT_SQL = "INSERT INTO announcement VALUES (default,default,?, ?)";
     private static final String SELECT_SQL = "SELECT * FROM announcement";
     private static final String UPDATE_SQL = "UPDATE announcement SET title = ?, contents = ? WHERE id = ?";
     private static final String DELETE_SQL = "DELETE announcement WHERE id =?";
     private static final String SELECT_BY_ID_SQL = "SELECT * FROM announcement WHERE id = ?";
-    
+
     public static AnnouncementDao getInstance() {
         return instance;
     }
-    
+
     public void create(Announcement announcement) throws SQLException {
         try (Connection connection = DataSource.getConnection()){
             try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_SQL)){
                 preparedStatement.setString(1, announcement.getTitle());
                 preparedStatement.setString(1, announcement.getTitle());
                 preparedStatement.setLong(3, announcement.getId());
-                
-                if(preparedStatement.executeUpdate() == 0) {
+
+                if (preparedStatement.executeUpdate() == 0) {
                     throw new SQLException();
                 }
                 connection.commit();
             }
         }
     }
+
+    public List<Announcement> read() throws SQLException{
+        List<Announcement> AnnouncementList = new ArrayList<>();
+        try (Connection connection = DataSource.getConnection()){
+            try (
+                    PreparedStatement preparedStatement = connection.prepareStatement(SELECT_SQL);
+                    ResultSet resultSet = preparedStatement.executeQuery();){
+                while (resultSet.next()) {
+                    Announcement announcement = Announcement.of(resultSet);
+                    AnnouncementList.add(announcement);
+                }
+            }
+        }
+        return Collections.unmodifiableList(AnnouncementList);
+    }
+    
+    public Announcement read(Long id) throws SQLException {
+        try (Connection connection = DataSource.getConnection()){
+            try (PreparedStatement preparedStatement = connection.prepareStatement(SELECT_BY_ID_SQL)){
+                preparedStatement.setLong(1, id);
+                try(ResultSet resultSet = preparedStatement.executeQuery()){
+                    if(resultSet.next()) {
+                        return Announcement.of(resultSet);
+                    }
+                }
+            }
+        }
+        throw new SQLException();
+    }
     
     
-    
+
 }
--- src/main/java/kr/co/khacademy/semi/model/Announcement.java
+++ src/main/java/kr/co/khacademy/semi/model/Announcement.java
@@ -1,5 +1,9 @@
 package kr.co.khacademy.semi.model;
 
+import java.sql.ResultSet;
+
+import javax.servlet.http.HttpServletRequest;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +17,38 @@ public class Announcement {
     Long accountId;
     String title;
     String contents;
+    
+    public static Announcement of(HttpServletRequest req) {
+        String title = req.getParameter("title");
+        String contents = req.getParameter("contents");
+        
+        if(!validateTitle(title)) {
+            throw new IllegalArgumentException();
+        }
+        return Announcement.builder()
+                .title(req.getParameter("title"))
+                .contents(req.getParameter("contents"))
+                .build();
+    }
+    
+    
+    private static boolean validateTitle(String title) {
+        return (6 <= title.length()) && (title.length() <= 100);
+    }
+
+
+    public static Announcement of(ResultSet resultSet) {
+        try {
+        return Announcement.builder()
+                .id(resultSet.getLong("id"))
+                .accountId(resultSet.getLong("account_id"))
+                .title(resultSet.getString("title"))
+                .contents(resultSet.getString("contents"))
+                .build();
+        }catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
     
     
 
