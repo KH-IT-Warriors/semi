8bef719c094e039aabb49fa044bdf52cf8b7a99c
Jang-Luke <lujke555@gmail.com>
2023-04-28 17:45:11 +0900

    feat: 관리자 -> 관리자 계정 삭제 기능 추가

--- src/main/java/kr/co/khacademy/semi/dao/UserDao.java
+++ src/main/java/kr/co/khacademy/semi/dao/UserDao.java
@@ -31,12 +31,12 @@ public class UserDao {
     private static final String SELECT_USER_SQL =
         "SELECT * FROM profiles P JOIN accounts A ON P.account_id = A.id WHERE id = ?";
     private static final String UPDATE_ACCOUNT_SQL =
-        "UPDATE accounts SET password = ? WHERE id = ?";
+        "UPDATE accounts SET status_id = ?, role_id = ?, password = ? WHERE id = ?";
     private static final String UPDATE_PROFILE_SQL =
         "UPDATE profiles SET name = ?, phoneNumber = ?, email = ?, mileage = ?, grade_id = ? WHERE account_id = ?";
-    private static final String DELETE_ACCOUNT_SQL =
+    private static final String DELETE_ADMIN_ACCOUNT_SQL =
         "DELETE FROM accounts WHERE id = ?";
-    private static final String DELETE_PROFILE_SQL =
+    private static final String DELETE_ADMIN_PROFILE_SQL =
         "DELETE FROM profiles WHERE account_id = ?";
 
 
@@ -96,8 +96,10 @@ public class UserDao {
     public void update(User user, Long targetId) throws SQLException {
         try (Connection connection = DataSource.getConnection()) {
             try(PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_ACCOUNT_SQL)){
-                preparedStatement.setString(1, user.getAccount().getPassword());
-                preparedStatement.setLong(2, targetId);
+                preparedStatement.setLong(1, user.getAccount().getStatusId());
+                preparedStatement.setLong(2, user.getAccount().getRoleId());
+                preparedStatement.setString(3, user.getAccount().getPassword());
+                preparedStatement.setLong(4, targetId);
                 if (preparedStatement.executeUpdate() == 0) {
                     connection.rollback();
                     throw new RuntimeException();
@@ -120,9 +122,22 @@ public class UserDao {
     }
 
     public void delete(Long targetId) throws SQLException {
-        try(Connection connection = DataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_PROFILE_SQL)){
-
+        try(Connection connection = DataSource.getConnection();){
+            try(PreparedStatement preparedStatement = connection.prepareStatement(DELETE_ADMIN_PROFILE_SQL)){
+                preparedStatement.setLong(1, targetId);
+                if (preparedStatement.executeUpdate() == 0) {
+                    connection.rollback();
+                    throw new RuntimeException();
+                }
+            }
+            try(PreparedStatement preparedStatement = connection.prepareStatement(DELETE_ADMIN_ACCOUNT_SQL)){
+                preparedStatement.setLong(1, targetId);
+                if (preparedStatement.executeUpdate() == 0) {
+                    connection.rollback();
+                    throw new RuntimeException();
+                }
+            }
+            connection.commit();
         }
-    } // TODO: 강제 삭제는 나중에 테이블이 다 만들어진 후에 만들기
+    } // TODO: 일반 유저 강제 삭제는 나중에 테이블이 다 만들어진 후에 만들기
 }
