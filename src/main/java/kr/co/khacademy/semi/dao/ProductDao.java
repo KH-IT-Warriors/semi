fb28c9ec2aee5c63fa6dfc4cd23bffa56471ebb3
park cho hye <chohye@172.20.nate.com>
2023-04-28 14:30:49 +0900

    ProductDao productReadList 추가

--- src/main/java/kr/co/khacademy/semi/dao/ProductDao.java
+++ src/main/java/kr/co/khacademy/semi/dao/ProductDao.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,123 +15,130 @@ import kr.co.khacademy.semi.model.Product;
 public class ProductDao {
 
     private static final ProductDao instance = new ProductDao();
-    
-    private static final String INSERT_PRODUT_SQL ="INSERT INTO product (name, title, summary, detail, price, quantity, categoryId) "
+
+    private static final String INSERT_PRODUT_SQL = "INSERT INTO product (name, title, summary, detail, price, quantity, categoryId) "
             + "VALUE (?, ?, ?, ?, ?, ?, ?)";
-    
+
+    private static final String SELECT_BY_ID_SQL = "SELECT * FROM product WHERE = ?";
+
+    private static final String SELECT_PRODUCT_SQL = "SELECT * FROM product";
+
     private static final String UPDATE_PRODUT_SQL = "UPDATE product SET name = ?, title = ?, summary = ?, detail = ?, "
             + "price = ?, quantity = ?, categoryId = ? WHERE = ? ";
-                                   
-   
-    private static final String SELECT_BY_ID_SQL = "SELECT * FROM product WHERE = ?";
-    
+
     private static final String DELETE_PRODUCT_SQL = "DELETE product WHERE = ?";
+
     
+    private ProductDao() {}
 
-    private ProductDao() {
-    }
-
+    
     public static ProductDao getInstance() {
         return instance;
     }
 
     public Boolean create(Product product) {
-        try (
-                Connection connection = DataSource.getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(INSERT_PRODUT_SQL)
-        ) {
-            preparedStatement.setString(1, product.getName());
-            preparedStatement.setString(2, product.getTitle());
-            preparedStatement.setString(3, product.getSummary());
-            preparedStatement.setString(4, product.getDetail());
-            preparedStatement.setLong(5, product.getPrice());
-            preparedStatement.setLong(6, product.getQuantity());
-            preparedStatement.setLong(7, product.getCategoryId());
-       
-            int result = preparedStatement.executeUpdate();
-            
-            if (result == 0) {
-                connection.rollback();
-                throw new SQLException();
+        try (Connection connection = DataSource.getConnection()) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_PRODUT_SQL)) {
+                preparedStatement.setString(1, product.getName());
+                preparedStatement.setString(2, product.getTitle());
+                preparedStatement.setString(3, product.getSummary());
+                preparedStatement.setString(4, product.getDetail());
+                preparedStatement.setLong(5, product.getPrice());
+                preparedStatement.setLong(6, product.getQuantity());
+                preparedStatement.setLong(7, product.getCategoryId());
+
+                int result = preparedStatement.executeUpdate();
+
+                if (result == 0) {
+                    connection.rollback();
+                    throw new SQLException();
+                }
             }
-        } catch (SQLException e) {
+        } catch (SQLException ignored) {
             return Boolean.FALSE;
         }
         return Boolean.TRUE;
- 
     }
 
-    
-    
     public Optional<Product> read(Long id) {
-      try (
-              Connection connection = DataSource.getConnection();
-              PreparedStatement preparedStatement = connection.prepareStatement(SELECT_BY_ID_SQL)
-              
-       ) {
-          preparedStatement.setLong(1, id);
-          try (ResultSet resultSet = preparedStatement.executeQuery()) {
-          resultSet.next();
-          return Optional.ofNullable(Product.of(resultSet));
-         }  
-      } catch (SQLException e) {
-         throw new IllegalArgumentException();
-      }
-      
+        try (Connection connection = DataSource.getConnection()) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(SELECT_BY_ID_SQL)) {
+                preparedStatement.setLong(1, id);
 
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        return Optional.of(Product.of(resultSet));
+                    }
+                }
+            }
+        } catch (SQLException ignored) {
+            }
+        return Optional.empty();
     }
+    
 
-    
-    
-    
     public List<Product> read() {
-        return null;
+        List<Product> products = new ArrayList<>();
+        try (Connection connection = DataSource.getConnection();) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(SELECT_PRODUCT_SQL)) {
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    while (resultSet.next()) {
+                        products.add(Product.of(resultSet));
+                    }
+                }
+
+            }
+
+        } catch (SQLException ignored) {
+        
+            }
+        return Collections.unmodifiableList(products);
     }
 
     public boolean update(Product product) {
-      try( 
-              Connection connection = DataSource.getConnection();
-              PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_PRODUT_SQL);
-    ) {
-          preparedStatement.setString(1,product.getName());
-          preparedStatement.setString(2,product.getTitle());
-          preparedStatement.setString(3, product.getSummary());
-          preparedStatement.setString(4, product.getDetail());
-          preparedStatement.setLong(5, product.getPrice());
-          preparedStatement.setLong(6, product.getQuantity());
-          preparedStatement.setLong(7, product.getCategoryId());
-          preparedStatement.setLong(8, product.getId());
-          
-          int result = preparedStatement.executeUpdate();
-          
-          if(result == 0) {
-              connection.rollback();
-              throw new SQLException();
-          }
-      } catch (SQLException e) {
-          return Boolean.FALSE;
-      }
-      return Boolean.TRUE;
+        try (Connection connection = DataSource.getConnection()) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_PRODUT_SQL);) {
+                preparedStatement.setString(1, product.getName());
+                preparedStatement.setString(2, product.getTitle());
+                preparedStatement.setString(3, product.getSummary());
+                preparedStatement.setString(4, product.getDetail());
+                preparedStatement.setLong(5, product.getPrice());
+                preparedStatement.setLong(6, product.getQuantity());
+                preparedStatement.setLong(7, product.getCategoryId());
+                preparedStatement.setLong(8, product.getId());
+
+                int result = preparedStatement.executeUpdate();
+
+                if (result == 0) {
+                    connection.rollback();
+                    throw new SQLException();
+                }
+            }
+        } catch (SQLException ignored) {
+            return Boolean.FALSE;
+        }
+        return Boolean.TRUE;
+    }
+    
+    
+    
+
+    public void delete(Long id) {
+        try (Connection connection = DataSource.getConnection()) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(DELETE_PRODUCT_SQL)) {
+                preparedStatement.setLong(1, id);
+                int result = preparedStatement.executeUpdate();
+
+                if (result == 0) {
+                    connection.rollback();
+                    throw new SQLException();
+                }
+
+            }
+
+        } catch (SQLException idnored) {
+
+        }
     }
 
-    
-    
-    public void delete(Long id) {
-      try (
-           Connection connection = DataSource.getConnection();
-           PreparedStatement preparedStatement = connection.prepareStatement(DELETE_PRODUCT_SQL)       
-       ) {
-          preparedStatement.setLong(1, id);
-          int result = preparedStatement.executeUpdate();
-          
-          if (result == 0) {
-             connection.rollback();
-             throw new SQLException();
-          }
-          
-      } catch (SQLException e) {
-        throw new IllegalArgumentException();
-      } 
-        
-    }
 }
