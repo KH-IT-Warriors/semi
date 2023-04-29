e8be699fadf2e94e5dc2195556812ae410f36b8e
Jang-Luke <lujke555@gmail.com>
2023-04-29 21:52:21 +0900

    feat: 일반 사용자 -> 회원가입 기능 구현

---
+++ src/main/java/kr/co/khacademy/semi/controller/AccountController.java
@@ -0,0 +1,37 @@
+package kr.co.khacademy.semi.controller;
+
+import kr.co.khacademy.semi.dao.UserDao;
+import kr.co.khacademy.semi.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/account/*")
+public class AccountController extends HttpServlet {
+
+    private static final UserDao userDao = UserDao.getInstance();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            String pathInfo = req.getPathInfo();
+            if ("/register".equals(pathInfo)) {
+                User user = User.of(req);
+                userDao.create(user);
+                req.setAttribute("registerSuccess", true);
+                req.getRequestDispatcher("index.jsp").forward(req, resp);
+            }
+        } catch (SQLException e) {
+            resp.sendRedirect("/error.jsp");
+        }
+    }
+}
