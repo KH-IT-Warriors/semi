ea3d6fe867265f5977ec33f4e32931c10dd3afec
park cho hye <chohye@172.20.nate.com>
2023-04-30 00:41:25 +0900

    ProductModel 유효성 검사 추가

--- src/main/java/kr/co/khacademy/semi/model/Product.java
+++ src/main/java/kr/co/khacademy/semi/model/Product.java
@@ -27,12 +27,47 @@ public class Product {
     
 
     public static Product of(HttpServletRequest req) {
+        
+        if(validateProductTitle(req.getParameter("title"))) {
+            throw new IllegalArgumentException();
+        }
+        
+        if(validateProductSummary(req.getParameter("summary"))) {
+            throw new IllegalArgumentException();
+        }
+        
+        if (validateProductDetail(req.getParameter("detail"))) {
+            throw new IllegalArgumentException();
+        }
+        
         return Product.builder()
+            .name(req.getParameter("name"))
+            .title(req.getParameter("title"))
+            .summary(req.getParameter("summary"))
+            .detail(req.getParameter("detail"))
+            .price(Long.valueOf(req.getParameter("price")))
+            .quantity(Long.valueOf(req.getParameter("quantity")))
+            .categoryId(Long.valueOf(req.getParameter("category_id")))
             .build();
     }
     
-   public static Product of (ResultSet resultSet) throws SQLException {
-         
+
+    
+    private static boolean validateProductTitle(String title) {   
+        return (title.length() >= 3) && (title.length() <= 200);
+    }
+    
+    private static boolean validateProductSummary(String summary) {
+        return (summary.length() >= 30);     
+    }
+    
+    private static boolean validateProductDetail (String detail) {
+        return (detail.length() >= 150);
+    }
+    
+  
+   public static Product of (ResultSet resultSet){
+       try {  
        return Product.builder()
                .id(resultSet.getLong("id"))
                .name(resultSet.getString("name"))
@@ -43,6 +78,8 @@ public class Product {
                .quantity(resultSet.getLong("quantity"))
                .categoryId(resultSet.getLong("category_id"))
                .build();    
-       
+       } catch (SQLException e) {
+           throw new IllegalArgumentException();
+       } 
    }
 }
