072bef55029c77bff118db26a1c75722e63647c8
park cho hye <chohye@172.20.nate.com>
2023-05-01 14:26:17 +0900

    ProductModel 수정

--- src/main/java/kr/co/khacademy/semi/model/Product.java
+++ src/main/java/kr/co/khacademy/semi/model/Product.java
@@ -17,48 +17,42 @@ public class Product {
 
     Long id;
     String name;
-    String title;
-    String summary;
-    String detail;
     Long price;
     Long quantity;
-    Long categoryId;
-    
-    
+    String category;
+    String summary;
+    String thumbnailImageUrl;
+    String detailImageUrl;
+
 
     public static Product of(HttpServletRequest req) {
-        
-        if(!validateProductTitle(req.getParameter("title"))) {
-            throw new IllegalArgumentException();
-        }
-        
+          
         return Product.builder()
             .name(req.getParameter("name"))
-            .title(req.getParameter("title"))
-            .summary(req.getParameter("summary"))
-            .detail(req.getParameter("detail"))
             .price(Long.valueOf(req.getParameter("price")))
             .quantity(Long.valueOf(req.getParameter("quantity")))
-            .categoryId(Long.valueOf(req.getParameter("category_id")))
+            .category(req.getParameter("category"))
+            .summary(req.getParameter("summary"))
+            .thumbnailImageUrl(req.getParameter("thumbnaile_imageurl"))
+            .detailImageUrl(req.getParameter("detail_imageurl"))
             .build();
+        
     }
     
     
-    private static Boolean validateProductTitle(String title) {   
-        return (3 <= title.length() ) && (title.length() <= 200);
-    }
+   
      
    public static Product of (ResultSet resultSet) throws SQLException{
       
        return Product.builder()
                .id(resultSet.getLong("id"))
                .name(resultSet.getString("name"))
-               .title(resultSet.getString("title"))
-               .summary(resultSet.getString("summary"))
-               .detail(resultSet.getString("detail"))
                .price(resultSet.getLong("price"))
                .quantity(resultSet.getLong("quantity"))
-               .categoryId(resultSet.getLong("category_id"))
+               .category(resultSet.getString("category"))
+               .summary(resultSet.getString("summary"))
+               .thumbnailImageUrl(resultSet.getString("thumbnaile_imageurl"))
+               .detailImageUrl(resultSet.getString("detail_imageurl"))
                .build();    
    
    }
