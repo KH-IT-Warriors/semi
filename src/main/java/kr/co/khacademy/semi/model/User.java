aec54a6940eb4c2de353594c64424e5bd9161f9a
Jang-Luke <lujke555@gmail.com>
2023-05-01 11:45:50 +0900

    feat: Account, Profile, User dto 객체 생성

---
+++ src/main/java/kr/co/khacademy/semi/model/User.java
@@ -0,0 +1,36 @@
+package kr.co.khacademy.semi.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+@Value
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class User {
+
+    Account account;
+    Profile profile;
+
+    public static User of(HttpServletRequest req) {
+        return User.builder()
+            .account(Account.of(req))
+            .profile(Profile.of(req))
+            .build();
+    }
+
+    public static User of(ResultSet resultSet) throws SQLException {
+        Account account = Account.of(resultSet);
+        Profile profile = Profile.of(resultSet);
+        return User.builder()
+            .account(account)
+            .profile(profile)
+            .build();
+    }
+}
